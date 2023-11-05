2c102a63a3f35c012d7f37b158503b0706764c4b
dima <melnik.dimoss@gmail.com>
2023-11-05 12:31:20 +0200

    change name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='sqlalchemy_catalyst',
+    name='sqlalchemy_core_catalyst',
     version='0.1',
     packages=find_packages(),
     install_requires=[
